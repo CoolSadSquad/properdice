b6536ad7418848bb9f7d88f101229ab175707d29
CornaElicottero <kizir2001@gmail.com>
2023-01-05 19:28:36 +0300

    123
    123

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const app = new Vue({
     el: '#app',
     data:{
-        username: 'zageev'
+        username: 'zageev',
+        cubicFirst: Math.random(1,6),
+        cubicSecond: Math.random (1,6)
+        
     }
 })
