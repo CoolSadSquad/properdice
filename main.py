22a4120afadc94068307b28e441d3a434dda79bd
verkhoglyadov <name1337sviter@gmail.com>
2023-01-08 08:21:57 +0500

    Added FastAPI

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-def main():
-    pass
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+html = open("index.html", "r").read()
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
