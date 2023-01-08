f74c6d5a890e1b1dfce21a28c3979d5696f85a28
zageev <name1337sviter@gmail.com>
2023-01-08 16:31:24 +0500

    Added FastAPI

---
+++ app/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, WebSocket, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+from pathlib import Path
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent.parent.absolute() / "static"),
+    name="static",
+)
+
+
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request}
+    )
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, log_level="info")
