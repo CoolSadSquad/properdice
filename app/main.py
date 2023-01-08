07380dacc29eaeca00fe8256e6c27e0d8a2174c0
zageev <name1337sviter@gmail.com>
2023-01-08 16:47:26 +0500

    updated .gitignore

--- app/main.py
+++ app/main.py
@@ -22,12 +22,12 @@ async def root(request: Request):
     )
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket):
+#     await websocket.accept()
+#     while True:
+#         data = await websocket.receive_text()
+#         await websocket.send_text(f"Message text was: {data}")
 
 
 if __name__ == "__main__":
